d6d3b0b9c4fac85fb79de4055100eccc2303772d
wanglei <wanglei@allinpay.com>
2017-07-03 10:09:03 +0800

    添加注释

--- src/main/java/orders/config/Neo4jConfig.java
+++ src/main/java/orders/config/Neo4jConfig.java
@@ -8,16 +8,18 @@ import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
 @Configuration
+// 扫描Repository所在包
 @EnableNeo4jRepositories(basePackages = "orders.db")
 public class Neo4jConfig extends Neo4jConfiguration {
 
 	public Neo4jConfig() {
+		// 扫描模型所在包
 		setBasePackage("orders");
 	}
 
-	@Bean(destroyMethod="shutdown")
-	public GraphDatabaseService graphDatabaseService() {	
-		return new GraphDatabaseFactory()
-				.newEmbeddedDatabase("/tmp/graphdb");
+	@Bean(destroyMethod = "shutdown")
+	public GraphDatabaseService graphDatabaseService() {
+		// 内嵌式
+		return new GraphDatabaseFactory().newEmbeddedDatabase("/tmp/graphdb");
 	}
 }
